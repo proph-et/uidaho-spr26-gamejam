900d6273697de39ad7554aed5eee1909b8fcdfb3
Hadix05 <meye4747@vandals.uidaho.edu>
2026-02-27 23:28:56 -0800

    updated enemies, working pathfinding

---
+++ scripts/enemies/enemy.gd
@@ -0,0 +1,12 @@
+extends PathFollow2D
+
+@export var speed:float = 100.0 
+
+func _process(delta:float) -> void:
+  progress += delta * speed
+  if progress_ratio >= 1.0:
+    var enemy = get_child(0)
+    
+    if enemy and enemy.has_method("die"):
+      print("hit end of line, dying")
+      enemy.die()
