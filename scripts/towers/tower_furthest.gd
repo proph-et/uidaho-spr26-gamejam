e765e42ccf2c67b1baa0d8cbf5116f1147f7aefd
Yoshiinator <joshuawerner98@outlook.de>
2026-03-01 13:30:15 -0800

    implement of the upgrade system
    pray kids

--- scripts/towers/tower_furthest.gd
+++ scripts/towers/tower_furthest.gd
@@ -1,12 +1,13 @@
 extends TowerParent
+class_name FurthestTower
 
 #base values 
 @export var damage: float = 60
 @export var attack_cooldown_s: float = 2.0
 @export var attack_range: float = 200.0:
-  set(value):
-    attack_range = maxf(value, 0.0)
-    _update_attack_collision_size()
+    set(value):
+        attack_range = maxf(value, 0.0)
+        _update_attack_collision_size()
 @export var target_mode: TargettingMode = TargettingMode.FURTHEST
 
 
@@ -19,17 +20,26 @@ var damage_upgrade_points := 0
 var cooldown_upgrade_points := 0
 var range_upgrade_points := 0
 
-func upgrade_cooldown() -> void:
+func upgrade_1() -> void:
+    if damage_upgrade_points < MAX_DAMAGE_UPGRADE:
+        damage_upgrade_points += 1
+        damage -= damage_upgrade_points * 10
+
+func upgrade_2() -> void:
     if cooldown_upgrade_points < MAX_COOLDOWN_UPGRADE:
         cooldown_upgrade_points += 1
-        attack_cooldown_s -= cooldown_upgrade_points * 0.3
+        attack_cooldown_s += cooldown_upgrade_points * 0.3
 
-func upgrade_damage() -> void:
-    if value_upgrade_points < MAX_VALUE_UPGRADE:
-        damage_upgrade_points += 1
-        damage += damage_upgrade_points * 10
-
-func upgrade_range() -> void:
-    if value_upgrade_points < MAX_VALUE_UPGRADE:
+func upgrade_3() -> void:
+    if range_upgrade_points < MAX_RANGE_UPGRADE:
         range_upgrade_points += 1
         attack_range += range_upgrade_points * 20
+
+func can_upgrade_1() -> bool:
+    return damage_upgrade_points < MAX_DAMAGE_UPGRADE
+
+func can_upgrade_2() -> bool:
+    return cooldown_upgrade_points < MAX_COOLDOWN_UPGRADE
+
+func can_upgrade_3() -> bool:
+    return range_upgrade_points < MAX_RANGE_UPGRADE
