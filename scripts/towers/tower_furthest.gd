10ae5df45ec1b9f4e2bf726752f3af2fe7612ee3
Yoshiinator <joshuawerner98@outlook.de>
2026-02-28 21:09:40 -0800

    Tower_changes
    tower abilities

---
+++ scripts/towers/tower_furthest.gd
@@ -0,0 +1,35 @@
+extends TowerParent
+
+#base values 
+@export var damage: float = 60
+@export var attack_cooldown_s: float = 2.0
+@export var attack_range: float = 200.0:
+  set(value):
+    attack_range = maxf(value, 0.0)
+    _update_attack_collision_size()
+@export var target_mode: TargettingMode = TargettingMode.FURTHEST
+
+
+# max upgrade points 
+const MAX_DAMAGE_UPGRADE := 3
+const MAX_COOLDOWN_UPGRADE := 3
+const MAX_RANGE_UPGRADE := 3
+
+var damage_upgrade_points := 0
+var cooldown_upgrade_points := 0
+var range_upgrade_points := 0
+
+func upgrade_cooldown() -> void:
+    if cooldown_upgrade_points < MAX_COOLDOWN_UPGRADE:
+        cooldown_upgrade_points += 1
+        attack_cooldown_s -= cooldown_upgrade_points * 0.3
+
+func upgrade_damage() -> void:
+    if value_upgrade_points < MAX_VALUE_UPGRADE:
+        damage_upgrade_points += 1
+        damage += damage_upgrade_points * 10
+
+func upgrade_range() -> void:
+    if value_upgrade_points < MAX_VALUE_UPGRADE:
+        range_upgrade_points += 1
+        attack_range += range_upgrade_points * 20
